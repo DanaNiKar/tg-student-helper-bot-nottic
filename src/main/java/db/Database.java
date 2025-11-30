98a62a975834097999b3efba08f63b45daae85f4
Danak <danakaropchuk21111@gmail.com>
2025-11-30 22:03:11 +0700

    implemented sqlite database class

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -1,4 +1,59 @@
 package db;
 
+import java.sql.*;
+
 public class Database {
+  private static final String DB_URL = "jdbc:sqlite:data.db";
+
+  public static Connection getConnection() throws SQLException {
+    return DriverManager.getConnection(DB_URL);
+  }
+
+  public static void init() {
+    try (Connection c = getConnection();
+    Statement s = c.createStatement()) {
+
+      // tasks
+      s.executeUpdate("""
+          CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            chat_id INTEGER,
+            title TEXT,
+            description TEXT,
+            deadline_ts INTEGER,
+            done INTEGER DEFAULT 0
+          );
+          """);
+
+      // events
+      s.executeUpdate("""
+          CREATE TABLE IF NOT EXISTS events (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            chat_id INTEGER,
+            title TEXT,
+            description TEXT,
+            event_ts INTEGER,
+            periodic INTEGER DEFAULT 0
+          );
+          """);
+
+      // schedule
+      s.executeUpdate("""
+          CREATE TABLE IF NOT EXISTS schedule (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            chat_id INTEGER,
+            day_of_week INTEGER,
+            time TEXT,
+            title TEXT,
+            location TEXT,
+            active INTEGER DEFAULT 1
+          );
+          """);
+
+      System.out.println("База данных инициализирована");
+    } catch (SQLException ex) {
+      ex.printStackTrace();
+      throw new RuntimeException(ex);
+    }
+  }
 }
