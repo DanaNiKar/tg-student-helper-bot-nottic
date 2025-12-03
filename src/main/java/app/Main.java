12b349238edd37e4ce82f0a10be67339bbf641e1
Danak <danakaropchuk21111@gmail.com>
2025-12-03 21:00:02 +0700

    added Main class

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -8,5 +8,24 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
   public static void main(String[] args) throws Exception {
+    Database.init();
+
+    ReminderService reminderService = ReminderService.getInstance();
+
+    TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+    String botToken = System.getenv("TG_BOT_TOKEN");
+    String botUsername = System.getenv("TG_BOT_USERNAME");
+
+    if (botToken == null || botUsername == null) {
+      System.err.println("Please set TG_BOT_TOKEN AND TG_BOT_USERNAME environment variables");
+      System.exit(1);
+    }
+
+    StudentHelperBot bot = new StudentHelperBot(botUsername, botToken, reminderService);
+    botsApi.registerBot(bot);
+
+    reminderService.reloadAllReminders();
+
+    System.out.println("Bot started");
   }
 }
